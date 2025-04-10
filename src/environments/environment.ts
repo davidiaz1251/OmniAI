c8829d8841f6b0036b176e953776e0a12cd4007e
Luis David Diaz Ramirez <luis.david.10@hotmail.com>
2025-04-10 10:49:19 +0200

    fix: update backend URL in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     urlOpenIA: 'https://api.openai.com/v1/chat/completions',
-    urlBackend: 'https://omni-ai-back.vercel.app/api/analyze',
+    urlBackend: 'https://omniai-back.onrender.com/api/analyze',
   };
