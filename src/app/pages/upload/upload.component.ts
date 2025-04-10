77465125743657409dcd33f78e0d57ae380499ed
Luis David Diaz Ramirez <luis.david.10@hotmail.com>
2025-04-10 20:30:54 +0200

    feat: enhance upload component to display invoice and product details

--- src/app/pages/upload/upload.component.ts
+++ src/app/pages/upload/upload.component.ts
@@ -8,10 +8,25 @@ import { FluidModule } from 'primeng/fluid';
 import { MessagesModule } from 'primeng/messages';
 import { ToastModule } from 'primeng/toast';
 import { OpenaiService } from '../../core/services/openai.service';
+import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
 
 @Component({
   selector: 'app-upload',
-  imports: [FluidModule, ButtonModule, FileUploadModule, FormsModule, CommonModule, MessagesModule, ToastModule],
+  imports: [FluidModule, 
+    ButtonModule, 
+    FileUploadModule, 
+    FormsModule, 
+    CommonModule, 
+    MessagesModule, 
+    ToastModule, 
+    TableModule,
+    InputTextModule,
+    IconFieldModule,
+    InputIconModule
+  ],
   templateUrl: './upload.component.html',
   styleUrl: './upload.component.scss',
   providers: [MessageService],
@@ -21,38 +36,62 @@ export class UploadComponent {
   constructor(private messageService: MessageService,
     private openaiService: OpenaiService
   ) { }
-  resultado: string = '';
+  resultado: any = null;
+  msg: string = '';
   cargando = false;
-  
+
   onUpload(event: any) {
     const files = event.files as File[];
     if (!files || files.length === 0) return;
-  
+
     const file = files[0]; // Tomamos solo el primero por ahora
     const reader = new FileReader();
-  
+
     reader.onload = () => {
       const base64 = (reader.result as string).split(',')[1]; // quitar encabezado data:image/jpeg;base64,
       this.enviarAOpenAI(base64);
     };
-  
+
     reader.readAsDataURL(file);
   }
 
   enviarAOpenAI(base64: string) {
+    if (this.cargando) return;
     this.cargando = true;
-    this.resultado = 'Analizando imagen...';
+    this.msg = 'Analizando imagen...';
 
     this.openaiService.analizarImagen(base64).subscribe({
       next: (res: any) => {
         this.cargando = false;
-        this.resultado = res.choices[0].message.content;
+        console.log(res);
+
+        const texto = res.choices[0].message.content;
+        const inicio = texto.indexOf('{');
+        const fin = texto.lastIndexOf('}');
+
+        if (inicio !== -1 && fin !== -1) {
+          const soloJSON = texto.substring(inicio, fin + 1);
+          try {
+            this.resultado = JSON.parse(soloJSON);
+          } catch (e) {
+            this.resultado = null;
+            console.error('Error al parsear JSON:', e);
+          }
+        } else {
+          this.resultado = null;
+          console.error('No se encontró JSON');
+        }
+
       },
       error: (err) => {
         this.cargando = false;
-        this.resultado = 'Error al procesar la imagen.';
+        this.msg = 'Error al procesar la imagen.';
         console.error(err);
       }
     });
+  }
+
+  getInputValue(event: Event): string {
+    return (event.target as HTMLInputElement).value;
   }
 }
